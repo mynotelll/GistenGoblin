e0301a0809cda9ed78b0b41fb59bcee1992c0d71
mynotelll <p.lachmann@proton.me>
2024-12-04 10:47:54 +0100

    added a button for managing products

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1200,
-        height: 600
+        height: 700
     })
 
     win.loadFile('index.html')
